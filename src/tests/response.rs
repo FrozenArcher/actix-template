3370bed2f168c9d863be241deb7129f227c0e5c1
FrozenArcher <yhy242989105@hotmail.com>
2024-02-22 03:40:03 +0800

    add custom response test

--- src/tests/response.rs
+++ src/tests/response.rs
@@ -1,6 +1,7 @@
 use actix_web::{http::StatusCode, web, ResponseError};
+use serde::Serialize;
 
-use crate::response::{AppError, AppResponse, AppResult};
+use crate::response::{AppError, AppResponse, AppResult, OkModel};
 
 #[test]
 fn success() {
@@ -32,4 +33,27 @@ fn internal_server_error() {
         let resp = e.error_response();
         assert_eq!(resp.status(), StatusCode::INTERNAL_SERVER_ERROR);
     }
+}
+
+#[test]
+fn custom_response() {
+    #[derive(Serialize)]
+    struct RespBody {
+        a: i32,
+        b: f64,
+        msg: &'static str,
+    }
+    let web::Json(model) = AppResponse::Success(RespBody {
+        a: 1,
+        b: 3.1415,
+        msg: "Hello!",
+    })
+    .response()
+    .unwrap();
+    let OkModel { success, data } = model;
+    assert!(success);
+    let RespBody { a, b, msg } = data;
+    assert_eq!(a, 1);
+    assert_ne!(b, 9876.5432);
+    assert_eq!(msg, "Hello!");
 }
